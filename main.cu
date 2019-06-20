531c5455b4805f366442a3ad7b53e45ef0ecad6e
William Ledbetter <wgledbetter@crimson.ua.edu>
2019-06-20 15:56:22 -0500

    1D Stencil Example Kernel

--- main.cu
+++ main.cu
@@ -1,16 +1,48 @@
 #include <iostream>
 
+#define N 2560
+#define M 512
+#define BLOCK_SIZE (N/M)
+#define RADIUS 5
+
 __global__ void mykernel(void) {
 
 }
 
-__global__ void add(double *a, double *b, double *c){
+__global__ void add(double *a, double *b, double *c, int n){
     int idx = threadIdx.x + blockIdx.x * blockDim.x;
-    c[idx] = a[idx] + b[idx];
+    if(idx < n){
+        c[idx] = a[idx] + b[idx];
+    }
 }
 
-#define N 2560
-#define THREADS_PER_BLOCK 512
+__global__ void stencil_1D(int *in, int *out){
+    // This is similar to a convolution in the neural network sense
+
+    __shared__ int temp[BLOCK_SIZE + 2*RADIUS];  // Memory that is shared across threads within a block
+    int gindex = threadIdx.x + blockIdx.x * blockDim.x;  // Global index (in)
+    int lindex = threadIdx.x + RADIUS;  // Local index (temp)
+
+    // Read inputs into shared memory
+    temp[lindex] = in[gindex];
+    if(threadIdx.x < RADIUS){
+        temp[lindex - RADIUS] = in[gindex - RADIUS];
+        temp[lindex + BLOCK_SIZE] = in[gindex + BLOCK_SIZE];
+    }
+
+    // Synchronize (ensure all threads have run above code before any proceed to following)
+        // Necessary for filling temp
+    __syncthreads();
+
+    // Apply stencil
+    int result = 0;
+    for(int offset = -RADIUS; offset <= RADIUS; offset++){
+        result += temp[lindex + offset];
+    }
+
+    // Store
+    out[gindex] = result;
+}
 
 int main(void){
     mykernel<<<1,1>>>();
@@ -36,10 +68,10 @@ int main(void){
     cudaMemcpy(y, b, size, cudaMemcpyHostToDevice);
 
     // Run the 'add' kernel
-    add<<<N/THREADS_PER_BLOCK, THREADS_PER_BLOCK>>>(x, y, z);
+    add<<<N/M, M>>>(x, y, z, N);  // Asynchronous: CPU proceeds before this is finished
 
     // Copy answer from device to host
-    cudaMemcpy(c, z, size, cudaMemcpyDeviceToHost);
+    cudaMemcpy(c, z, size, cudaMemcpyDeviceToHost);  // Waits until all previous CUDA calls have completed
 
     // Clean device
     cudaFree(x);
