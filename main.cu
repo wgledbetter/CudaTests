8f0d27ffbe6f5a31c0ce11238eef6c3a199411f7
William Ledbetter <wgledbetter@crimson.ua.edu>
2019-06-20 14:39:18 -0500

    Vectorized addition with blocks

--- main.cu
+++ main.cu
@@ -5,40 +5,52 @@ __global__ void mykernel(void) {
 }
 
 __global__ void add(int *a, int *b, int *c){
-    *c = *a + *b;
+    c[blockIdx.x] = a[blockIdx.x] + b[blockIdx.x];
 }
+
+#define N 512
 
 int main(void){
     mykernel<<<1,1>>>();
     std::cout << "wow" << std::endl;
 
-    int a, b, c;  // Host variables
+    int *a, *b, *c;  // Host variables
     int *x, *y, *z;  // Device variables
-    int size = sizeof(int);
+    int size = N * sizeof(int);
 
     cudaMalloc((void **)&x, size);
     cudaMalloc((void **)&y, size);
     cudaMalloc((void **)&z, size);
 
-    a = 2;
-    b = 7;
+    // Set Input
+    a = (int *)malloc(size);
+    b = (int *)malloc(size);
+    c = (int *)malloc(size);
+    // std::random_ints(a, N);
+    // std::random_ints(b, N);
 
     // Copy from host to device
-    cudaMemcpy(x, &a, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(y, &b, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(x, a, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(y, b, size, cudaMemcpyHostToDevice);
 
     // Run the 'add' kernel
-    add<<<1,1>>>(x, y, z);
+    add<<<N,1>>>(x, y, z);
 
     // Copy answer from device to host
-    cudaMemcpy(&c, z, size, cudaMemcpyDeviceToHost);
+    cudaMemcpy(c, z, size, cudaMemcpyDeviceToHost);
 
     // Clean device
     cudaFree(x);
     cudaFree(y);
     cudaFree(z);
 
-    std::cout << "Answer: " << c << std::endl;
+    // Clean Host
+    free(a);
+    free(b);
+    free(c);
+
+
+    std::cout << "Answer: " << c[23] << std::endl;
 
     return 12;
 }
