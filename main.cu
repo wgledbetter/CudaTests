e1e9d6d456683f2e2317ac7f02609277e80848f4
William Ledbetter <wgledbetter@crimson.ua.edu>
2019-06-22 16:31:42 -0500

    Cuda header

--- main.cu
+++ main.cu
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <stdio.h>
 
+#include "MyClass.cuh"
+
 #define N 2560
 #define M 512
 #define BLOCK_SIZE (N/M)
@@ -26,55 +28,6 @@ struct MyStruct {
 
     int x;
     double y;
-};
-
-class MyClass {
-    public:
-        double hostParam;
-        double *devParam;
-        const size_t dubSize = sizeof(double);
-
-        int nCudaBlocks = 1;
-        int nCudaThreadsPerBlock = 1;
-
-        MyClass(){
-            cudaMalloc((void **)&devParam, dubSize);
-        }
-
-        ~MyClass(){
-            cudaFree(devParam);
-        }
-
-        void set_param(double in){
-            hostParam = in;
-            cudaMemcpy(devParam, &hostParam, dubSize, cudaMemcpyHostToDevice);
-        }
-
-        double do_it_on_host(){
-            double out;
-            hostKernel(&hostParam, &out);
-            return out;
-        }
-
-        double do_it_on_device(){
-            double *devOut, out;
-            cudaMalloc((void **)&devOut, dubSize);
-            devKernel<<< nCudaBlocks, nCudaThreadsPerBlock >>>(devParam, devOut);
-            cudaMemcpy(&out, devOut, dubSize, cudaMemcpyDeviceToHost);
-            return out;
-        }
-
-        __global__ static void devKernel(double *param, double *ans){
-            // Cuda implementation
-            std::printf("Inside devKernel: ");
-            *ans = *param + 3.14;
-        }
-
-        void hostKernel(double *param, double *ans){
-            // Host implementation
-            std::cout << "Inside hostKernel: " << "wow" << std::endl;
-            *ans = *param + 3.14;
-        }
 };
 
 __global__ void add(double *a, double *b, double *c, int n){
